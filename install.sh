64bb1866ba4977d0268de80949b09f81d7758657
RE43P3R <87203697+ArkAngeL43@users.noreply.github.com>
2022-02-21 01:20:02 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,6 @@
+go get github.com/bndr/gotabulate
+go get github.com/flopp/go-staticmaps
+go get github.com/fogleman/gg
+go get github.com/golang/geo/s2
+go get github.com/rwcarlsen/goexif/exif
+go get github.com/spf13/pflag
